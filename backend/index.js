6f2b8f857e5614a23a3b2e1dc20c43523e06d078
h0996g <h0996g@gmail.com>
2024-07-14 14:39:28 +0100

    chore: Remove unused files and dependencies, update routing in App.js

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,8 @@ app.get('/api/chat', (req, res) => {
     res.send(chats);
 }
 );
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 5000');
 }
 );
